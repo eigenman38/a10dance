e3b9fa898fd17e513bcaddbf1b6812faedc62091
eigenman <eigenman38@gmail.com>
2022-04-13 16:09:48 -0600

    finished

--- src/app/roster/roster.page.ts
+++ src/app/roster/roster.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActionSheetController } from '@ionic/angular';
+import { ActionSheetController, AlertController, ToastController } from '@ionic/angular';
 import { Student } from '../student';
 import { StudentsService } from '../students.service';
 
@@ -13,20 +13,52 @@ export class RosterPage implements OnInit {
   students: Student[] = [];
 
   constructor(private studentService: StudentsService,
-    private actionSheetController: ActionSheetController) { }
+    private actionSheetController: ActionSheetController,
+    private alertController: AlertController,
+    private toastController: ToastController) { }
 
   ngOnInit() {
     this.students = this.studentService.getAll();
   }
 
+  async presentDeleteAlert(student: Student) {
+    const alert = await this.alertController.create({
+      header: 'Delete this student?',
+      subHeader: `${student.firstName} ${student.lastName}`,
+      message: 'This operation cannot be undone.',
+      buttons: [
+
+        {
+          text: 'Delete',
+          handler: () => this.deleteStudent(student)
+        },
+
+        {
+          text: 'Never Mind',
+          role: 'cancel'
+        }
+      ]
+    });
+    await alert.present();
+
+  }
+
   async deleteStudent(student: Student) {
     this.students = this.students.filter(x => x.id !== student.id);
+
+    const alert = await this.toastController.create({
+      message: `${student.firstName} ${student.lastName} deleted.`,
+      position: 'top',
+      duration: 3000
+    });
+
+    await alert.present();
 
   }
 
   async presentActionSheet(student: Student) {
     const actionSheet = await this.actionSheetController.create({
-      header: `${student.firstName}${student.lastName}`,
+      header: `${student.firstName} ${student.lastName}`,
       buttons: [{
         text: 'Mark Present',
         icon: 'eye',
@@ -48,7 +80,7 @@ export class RosterPage implements OnInit {
         icon: 'trash',
         role: 'destructive',
         handler: () => {
-          this.deleteStudent(student);
+          this.presentDeleteAlert(student);
         }
       },
 
