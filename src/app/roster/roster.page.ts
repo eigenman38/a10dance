a438ddbe75336dc239865f566d91f9a512b7b955
eigenman <eigenman38@gmail.com>
2022-04-12 20:44:24 -0600

    roster created

---
+++ src/app/roster/roster.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Student } from '../student';
+import { StudentsService } from '../students.service';
+
+@Component({
+  selector: 'app-roster',
+  templateUrl: './roster.page.html',
+  styleUrls: ['./roster.page.scss'],
+})
+export class RosterPage implements OnInit {
+
+  students: Student[] = [];
+
+  constructor(private studentService: StudentsService) { }
+
+  ngOnInit() {
+    this.students = this.studentService.getAll();
+  }
+
+}
